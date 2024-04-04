f18a13eb30b067522fd725ef21c20404ef28b5ac
yule <yulefang77@gmail.com>
2024-04-04 11:41:19 +0800

    Change while condition to i >0

--- passwd-retry.py
+++ passwd-retry.py
@@ -1,15 +1,16 @@
 i = 3
 password = 'qwe123'
 
-while True:
+while i > 0:
 	check = input('請輸入密碼: ')
+	i = i - 1
 	if check == password:
 		print('成功登入')
 		break
 	else:
-		i = i - 1
+		print('密碼錯誤！')
 		if i > 0:
-			print('密碼錯誤！你還有', i, ' 次機會')
+			print('你還有', i, ' 次機會')
 		else:
-			print('密碼錯誤！你的帳號已被鎖住，請連絡管理員。')
-			break
+			print('你的帳號已被鎖住，請連絡管理員。')
+			break 
